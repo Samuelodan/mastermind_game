f5fac660e88d64f2dcd5af2caa933935f37ccf04
Samuel ODaniels <samodan195@gmail.com>
2022-05-16 14:12:12 +0100

    Add magic comment and Board class description

--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: false
+
+# creates new boards
 class Board
   attr_reader :all_colors
   attr_accessor :current_guess
@@ -6,4 +9,5 @@ class Board
     @all_colors = %w[red yellow green blue brown purple]
     @current_guess = nil
   end
+  
 end
