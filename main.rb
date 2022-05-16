6d11ab98d14dd8cf9fa8955eec31574235525db2
Samuel ODaniels <samodan195@gmail.com>
2022-05-16 14:30:34 +0100

    Add make_code method to Board class

--- main.rb
+++ main.rb
@@ -2,12 +2,22 @@
 
 # creates new boards
 class Board
-  attr_reader :all_colors
+  attr_reader :all_colors, :code
   attr_accessor :current_guess
 
   def initialize
     @all_colors = %w[red yellow green blue brown purple]
     @current_guess = nil
+    @code = nil
   end
-  
+
+  def make_code
+    random_code = []
+    while random_code.length < 4
+      random_code << all_colors.sample
+      random_code.uniq!
+    end
+    @code = random_code
+    random_code
+  end
 end
