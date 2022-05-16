a0eb7151f1b941f8dc91037f4d2ac23255557ad3
Samuel ODaniels <samodan195@gmail.com>
2022-05-16 19:24:47 +0100

    Add create_keys method to Board class

--- main.rb
+++ main.rb
@@ -27,4 +27,18 @@ class Board
     guess = gets.chomp.downcase.split(',')
     @current_guess = guess.map(&:strip)
   end
+
+  def create_keys
+    initial_match = @current_guess.select do |item|
+      @code.include?(item)
+    end
+    exact_match = @current_guess.select do |item|
+      @current_guess.index(item) == @code.index(item)
+    end
+
+    black_keys = exact_match.length
+    white_keys = initial_match.length - black_keys
+
+    { white_keys: white_keys, black_keys: black_keys }
+  end
 end
