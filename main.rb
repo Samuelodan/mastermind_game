73cd889d154f0538a66f828a2817200f865dba34
Samuel ODaniels <samodan195@gmail.com>
2022-05-16 14:10:32 +0100

    Add current_guess variable to Board initialize

--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
 class Board
+  attr_reader :all_colors
+  attr_accessor :current_guess
+
   def initialize
     @all_colors = %w[red yellow green blue brown purple]
+    @current_guess = nil
   end
-
 end
